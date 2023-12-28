3bcac6cfe171d8b83e0cc1b0d1c18d7c7a543426
eda3 <gerorin1010@gmail.com>
2023-12-28 20:02:35 +0000

    マクロ

--- ji実戦Rustプログラミング入門_20231228/ch03/p061-マクロ/src/main.rs
+++ ji実戦Rustプログラミング入門_20231228/ch03/p061-マクロ/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let _s = concat!("A", "b2", 3);
+    // _s = "Ab23"
+    dbg!(_s);
+
+    let _s = format!("{}-{:?}", _s, ("D", 5));
+    // _s = "Ab23-(\"D\", 5)"
+    dbg!(_s);
+
+    let _s = format!("{}{}", "abc", "def");
+    dbg!(_s);
+    _s = "abcdef"
 }
