a49e431c486f651531ade19b183ef9cacf14271b
eda3 <gerorin1010@gmail.com>
2024-08-18 02:11:25 +0900

    引数を受け取れるようにした

--- ji実戦Rustプログラミング入門_20231228/p145-samplecli/src/main.rs
+++ ji実戦Rustプログラミング入門_20231228/p145-samplecli/src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // コマンドライン引数をベクターに収集
+    let args: Vec<String> = env::args().collect();
+
+    // "HelloWorld"と出力
+    println!("HelloWorld");
+
+    // 収集したコマンドライン引数をデバッグ用に表示
+    println!("{:?}", args);
 }
