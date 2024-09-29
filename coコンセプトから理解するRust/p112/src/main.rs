e570058a80332ca861f49f7978943b8e9c82e1fc
eda3 <gerorin1010@gmail.com>
2024-09-29 16:53:58 +0900

    p112

---
+++ coコンセプトから理解するRust/p112/src/main.rs
@@ -0,0 +1,16 @@
+fn myclear(x: &mut String) {
+    x.clear();
+}
+
+fn main() {
+    let mut s = "Hello".to_string();
+    let s_ref = &mut s;
+
+    let s_ref = &mut s;
+    myclear(s_ref);
+    println!("s={}", s);
+
+    let s_ref2 = &mut s;
+    myclear(s_ref2);
+    println!("s={}", s);
+}
