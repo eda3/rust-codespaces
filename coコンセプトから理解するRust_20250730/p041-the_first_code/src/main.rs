2f8b477b76001d2424382858ad76fdeea107a0f0
eda3 <gerorin1010@gmail.com>
2025-07-30 21:28:46 +0900

    add func_ex_print_result()

--- coコンセプトから理解するRust_20250730/p041-the_first_code/src/main.rs
+++ coコンセプトから理解するRust_20250730/p041-the_first_code/src/main.rs
@@ -23,6 +23,13 @@ fn func_ex_div_some<T:std:fmt::Display>(ans: Option<T>) {
     }
 }
 
+fn func_ex_print_result<T: std::fmt:Display, E: std::fmt::Display>(ans: Result<T, E>) {
+    match ans {
+        Ok(res) => println!("{}", res),
+        Err(str) => println!("{}", str),
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
