cb35a4a6f45ec8a90e2881fcaf6d57b4a165f982
eda3 <gerorin1010@gmail.com>
2025-07-31 20:05:14 +0900

    add

--- coコンセプトから理解するRust_20250730/p041-the_first_code/src/main.rs
+++ coコンセプトから理解するRust_20250730/p041-the_first_code/src/main.rs
@@ -1,13 +1,9 @@
-fn func_ex_div_sum(x:i32, y:i32) -> Option<i32> {
-    let ans = if y == 0 {
-        None
-    } else {
-        Some(x / y)
-    };
+fn func_ex_div_sum(x: i32, y: i32) -> Option<i32> {
+    let ans = if y == 0 { None } else { Some(x / y) };
     ans
 }
 
-fn func_ex_div_result(x:i32, y:i32) -> Result<i32, &'static str> {
+fn func_ex_div_result(x: i32, y: i32) -> Result<i32, &'static str> {
     if y == 0 {
         Err("divide by zero")
     } else {
@@ -15,21 +11,32 @@ fn func_ex_div_result(x:i32, y:i32) -> Result<i32, &'static str> {
     }
 }
 
-fn func_ex_div_some<T:std>:fmt::Display>(ans: Option<T>) {
+fn func_ex_print_some<T: std::fmt::Display>(ans: Option<T>) {
     if let Some(x) = ans {
-        println!("{}", x);
+        println!("{x}");
     } else {
         println!("None");
     }
 }
 
-fn func_ex_print_result<T: std::fmt:Display, E: std::fmt::Display>(ans: Result<T, E>) {
+fn func_ex_print_some_match<T: std::fmt::Display>(ans: Option<T>) {
     match ans {
-        Ok(res) => println!("{}", res),
-        Err(str) => println!("{}", str),
+        Some(x) => println!("{x}"),
+        None => println!("None"),
+    }
+}
+
+fn func_ex_print_result<T: std::fmt::Display, E: std::fmt::Display>(ans: Result<T, E>) {
+    match ans {
+        Ok(res) => println!("{res}"),
+        Err(str) => println!("{str}"),
     }
 }
 
 fn main() {
     func_ex_print_some(func_ex_div_sum(10, 5));
+    func_ex_print_some(func_ex_div_sum(10, 0));
+    func_ex_print_some_match(func_ex_div_sum(10, 5));
+    func_ex_print_some_match(func_ex_div_sum(10, 0));
+    func_ex_print_result(func_ex_div_result(10, 5));
 }
