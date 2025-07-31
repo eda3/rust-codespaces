e40379f1b61e96e4240d57e4c4d6fbf261b6ce3b
eda3 <gerorin1010@gmail.com>
2025-07-31 20:15:59 +0900

    comment

--- coコンセプトから理解するRust_20250730/p041-the_first_code/src/main.rs
+++ coコンセプトから理解するRust_20250730/p041-the_first_code/src/main.rs
@@ -1,8 +1,12 @@
+// Divide x by y.
+// If y is 0, return "None" (no value); otherwise return the result inside Some.
 fn func_ex_div_sum(x: i32, y: i32) -> Option<i32> {
     let ans = if y == 0 { None } else { Some(x / y) };
     ans
 }
 
+// Divide x by y.
+// If y is 0, return an error message; otherwise return the result.
 fn func_ex_div_result(x: i32, y: i32) -> Result<i32, &'static str> {
     if y == 0 {
         Err("divide by zero")
@@ -11,6 +15,8 @@ fn func_ex_div_result(x: i32, y: i32) -> Result<i32, &'static str> {
     }
 }
 
+// Print the value inside an Option.
+// If it is Some, print the number; if it is None, print "None".
 fn func_ex_print_some<T: std::fmt::Display>(ans: Option<T>) {
     if let Some(x) = ans {
         println!("{x}");
@@ -19,6 +25,7 @@ fn func_ex_print_some<T: std::fmt::Display>(ans: Option<T>) {
     }
 }
 
+// Same as adove, but uses a match statement.
 fn func_ex_print_some_match<T: std::fmt::Display>(ans: Option<T>) {
     match ans {
         Some(x) => println!("{x}"),
@@ -26,6 +33,8 @@ fn func_ex_print_some_match<T: std::fmt::Display>(ans: Option<T>) {
     }
 }
 
+// Print the value inside a Result.
+// If it is Ok, print the result; if it is err, print the error text.
 fn func_ex_print_result<T: std::fmt::Display, E: std::fmt::Display>(ans: Result<T, E>) {
     match ans {
         Ok(res) => println!("{res}"),
@@ -39,4 +48,5 @@ fn main() {
     func_ex_print_some_match(func_ex_div_sum(10, 5));
     func_ex_print_some_match(func_ex_div_sum(10, 0));
     func_ex_print_result(func_ex_div_result(10, 5));
+    func_ex_print_result(func_ex_div_result(10, 0));
 }
