8d418d2a5e361fcb06b27b60fe270b04e3a1dd24
eda3 <gerorin1010@gmail.com>
2025-07-31 20:29:23 +0900

    comment

--- coコンセプトから理解するRust_20250730/p054-test_code_in_rust/src/main.rs
+++ coコンセプトから理解するRust_20250730/p054-test_code_in_rust/src/main.rs
@@ -1,3 +1,21 @@
+// A simple function that adds tow i32 integers.
+fn add_i32(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+// Test case 1: 1 + 2 = 3 -> should pass.
+#[test]
+fn test1() {
+    assert_eq!(add_i32(1, 2), 3); // Success
+}
+
+// Test case 2: 2 + 4 = 6, but expected 7 => should fail
+#[test]
+fn test2() {
+    assert_eq!(add_i32(2, 4), 7) // Failure
+}
+
+// Main function for manual run: prints 2 + 5 = 7
 fn main() {
-    println!("Hello, world!");
+    println!("{}", add_i32(2, 5));
 }
