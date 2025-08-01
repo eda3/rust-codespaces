9040f633ff8f930030177a3db09bc45d38fbc426
eda3 <gerorin1010@gmail.com>
2025-08-02 05:40:14 +0900

    comment

--- coコンセプトから理解するRust_20250730/p107-once_bound/src/main.rs
+++ coコンセプトから理解するRust_20250730/p107-once_bound/src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
+    // Create a new String object from a string literal
     let s = "Hello".to_string();
+
+    // Move ownership of the String from `s` to `to`
     let t = s;
+
+    // This works: `t` now owns the String
     println!("{t}");
-    println!("{s}");
+
+    // This fails: `s` no longer owns the value (it's been moved)
+    // println!("{s}");
 }
