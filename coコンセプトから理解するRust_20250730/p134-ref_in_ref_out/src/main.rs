9cdaeea23d2ed3d57f73c769e7328736a8893b8f
eda3 <gerorin1010@gmail.com>
2025-08-02 10:15:54 +0900

    comment

--- coコンセプトから理解するRust_20250730/p134-ref_in_ref_out/src/main.rs
+++ coコンセプトから理解するRust_20250730/p134-ref_in_ref_out/src/main.rs
@@ -1,10 +1,16 @@
+// Define a function that takes a slice of i32s and returns a subslice
 fn pick1(x: &[i32], end: usize) -> &[i32] {
     &x[..end]
 }
 
 fn main() {
+    // Declare a fixed-size array of integers
     let v1 = [1, 2, 3, 4, 5];
-    let p = pick1(&v1, 2);
+
+    // Call the funtion with a reference to the array and an index value
+    let p = pick1(&v1, 2); // 'p' now refers to the first two elements: &[1, 2]
+
+    // Iterate over the returned slice and print each element
     for ss in p {
         println!("{ss}");
     }
